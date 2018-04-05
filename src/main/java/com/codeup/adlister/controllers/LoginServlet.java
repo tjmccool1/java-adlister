098d1ea1f55ca4d0e79e9e2d5cf7a173a389bd12
TMcCool <tjmccool1@icloud.com>
2018-04-05 09:32:17 -0500

    stashed

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,8 +1,5 @@
 package com.codeup.adlister.controllers;
 
-import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +9,7 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         if (request.getSession().getAttribute("user") != null) {
             response.sendRedirect("/profile");
             return;
@@ -23,17 +18,11 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
         // TODO: find a record in your database that matches the submitted password
-        User user = DaoFactory.getUsersDao().findByUsername(username);
         // TODO: make sure we find a user with that username
-        if (user == null) {
-            response.sendRedirect("/login");
-            return;
-        }
         // TODO: check the submitted password against what you have in your database
         boolean validAttempt = false;
 
